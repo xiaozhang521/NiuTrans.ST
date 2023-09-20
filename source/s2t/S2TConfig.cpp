730f51478f5fcd11110d47bef96c5aba394cbb9f
Kaiqi Kou <Koukq0907@gmail.com>
2023-09-20 21:06:04 +0800

    Create S2TConfig.cpp
    upload S2TConfig.cpp

---
+++ source/s2t/S2TConfig.cpp
@@ -0,0 +1,144 @@
+/* NiuTrans.S2T - an open-source speech-to-text system.
+ * Copyright (C) 2020 NiuTrans Research. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+ /*
+  * $Created by: Yuhao Zhang (yoohao.zhang@gmail.com) 2023-09-19
+  *
+  */
+
+#include <fstream>
+#include "S2TConfig.h"
+
+using namespace nts;
+using namespace std;
+
+/* the s2t namespace */
+namespace s2t
+{   
+    /*
+    load configurations from the command
+    >> argc - number of arguments
+    >> argv - the list of arguments
+    */
+    S2TConfig::S2TConfig(int argc, const char** argv)
+    {
+        char** args = new char* [MAX_PARAM_NUM];
+        for (int i = 0; i < argc; i++) {
+            args[i] = new char[strlen(argv[i]) + 1];
+            strcpy(args[i], argv[i]);
+        }
+        for (int i = argc; i < MAX_PARAM_NUM; i++) {
+            args[i] = NULL;
+        }
+
+        char* configFN = new char[1024];
+        LoadParamString(argc, args, "config", configFN, "");
+
+        int argsNum = argc;
+
+        /* override the configuration according to the file content */
+        if (strcmp(configFN, "") != 0)
+            argsNum = LoadFromFile(configFN, args);
+
+        /* parse configuration in args */
+        model.Load(argsNum, (const char **)args);
+        common.Load(argsNum, (const char **)args);
+        training.Load(argsNum, (const char **)args);
+        inference.Load(argsNum, (const char **)args);
+
+        for (int i = 0; i < MAX(argc, argsNum); i++)
+            delete[] args[i];
+        delete[] args;
+        delete[] configFN;
+    }
+
+    /*
+    load configurations from a file
+    >> configFN - path to the configuration file
+    >> args - the list to store the configurations
+    << argsNum - the number of arguments
+    format: one option per line, separated by a blank or a tab
+    */
+    int S2TConfig::LoadFromFile(const char* configFN, char** args) 
+    {
+        ifstream f(configFN, ios::in);
+        CheckNTErrors(f.is_open(), "Failed to open the config file");
+
+        int argsNum = 0;
+
+        /* parse arguments from the file */
+        string key, value;
+        while (f >> key >> value && argsNum < (MAX_PARAM_NUM - 1)) {
+            if (args[argsNum] != NULL) {
+                delete[] args[argsNum];
+            }
+            if (args[argsNum + 1] != NULL) {
+                delete[] args[argsNum + 1];
+            }
+            args[argsNum] = new char[1024];
+            args[argsNum + 1] = new char[1024];
+            strcpy(args[argsNum++], key.c_str());
+            strcpy(args[argsNum++], value.c_str());
+        }
+
+        /* record the number of arguments */
+        return argsNum;
+    }
+
+    /* load s2t model configuration from the command */
+    S2TModelConfig::Load(int argsNum, const char** args)
+    {
+        Create(argsNum, args);
+
+        LoadBool("bigatt", &useBigAtt, false);
+        LoadBool("encprenorm", &encPreLN, true);
+        LoadBool("decprenorm", &decPreLN, true);
+        LoadBool("encl1norm", &encoderL1Norm, false);
+        LoadBool("decl1norm", &decoderL1Norm, false);
+        LoadBool("decoderonly", &decoderOnly, false);
+        LoadBool("enchistory", &useEncHistory, false);
+        LoadBool("dechistory", &useDecHistory, false);
+        LoadBool("encfinalnorm", &encFinalNorm, true);
+        LoadBool("decfinalnorm", &decFinalNorm, true);
+        LoadBool("shareencdec", &shareEncDecEmb, false);
+        LoadBool("sharedec", &shareDecInputOutputEmb, false);
+
+        LoadInt("fbank", &fbank, 80)
+        LoadInt("pad", &pad, -1);
+        LoadInt("sos", &sos, -1);
+        LoadInt("eos", &eos, -1);
+        LoadInt("unk", &unk, -1);
+        LoadInt("encemb", &encEmbDim, 512);
+        LoadInt("decemb", &decEmbDim, 512);
+        LoadInt("maxsrc", &maxSrcLen, 200);
+        LoadInt("maxtgt", &maxTgtLen, 200);
+        LoadInt("enclayer", &encLayerNum, 6);
+        LoadInt("declayer", &decLayerNum, 6);
+        LoadInt("maxrp", &maxRelativeLength, -1);
+        LoadInt("encffn", &encFFNHiddenDim, 1024);
+        LoadInt("decffn", &decFFNHiddenDim, 1024);
+        LoadInt("srcvocabsize", &srcVocabSize, -1);
+        LoadInt("tgtvocabsize", &tgtVocabSize, -1);
+        LoadInt("encheads", &encSelfAttHeadNum, 4);
+        LoadInt("decheads", &decSelfAttHeadNum, 4);
+        LoadInt("encdecheads", &encDecAttHeadNum, 4);
+
+        LoadFloat("dropout", &dropout, 0.3F);
+        LoadFloat("ffndropout", &ffnDropout, 0.1F);
+        LoadFloat("attdropout", &attDropout, 0.1F);
+    }
+}
