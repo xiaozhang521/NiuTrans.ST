066413055374f0efe0094fece20e0f7e5eca717f
Kaiqi Kou <Koukq0907@gmail.com>
2023-10-12 16:48:12 +0800

    Update Main.cpp

--- source/Main.cpp
+++ source/Main.cpp
@@ -54,10 +54,11 @@ int main(int argc, const char** argv)
     // vocab.ShowVocab();
     vocab.Test();
 
-
     Generator generator;
     generator.Init(config, model);
-    generator.TestTranslate();
+    generator.TestInference();
+
+
     //generator.generate(); 
     // 
     /*****************************Old entrance******************************/
