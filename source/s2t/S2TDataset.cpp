3898d8e3cae168f87336429d92983dcbb11c3e1f
xiaozhang521 <yoohao.zhang@gmail.com>
2023-10-14 14:52:32 +0800

    Update functions of  S2T dataset.

--- source/s2t/S2TDataset.cpp
+++ source/s2t/S2TDataset.cpp
@@ -25,25 +25,152 @@
 
 using namespace nts;
 
+/* the s2t namespace */
 namespace s2t {
-    /* constructor */
-    TripleSample::TripleSample(FloatList* a, IntList* s, IntList* t, int myKey)
-    {
-        index = -1;
-        audioSeq = a;
-        srcSeq = s;
-        tgtSeq = t;
-        bucketKey = myKey;
-    }
 
-    /* de-constructor */
-    TripleSample::~TripleSample()
-    {
-        if (audioSeq != NULL)
-            delete audioSeq;
-        if (srcSeq != NULL)
-            delete srcSeq;
-        if (tgtSeq != NULL)
-            delete tgtSeq;
+/* get the maximum source sentence length in a range of buffer */
+int AudioDataSetBase::MaxAudioLen(int begin, int end) {
+    CheckNTErrors((end > begin) && (begin >= 0)
+        && (end <= buf->count), "Invalid range");
+    int maxLen = 0;
+    for (int i = begin; i < end; i++) {
+        TensorList* audioSeq = ((TripleSample*)buf->Get(i))->audioSeq;
+        maxLen = MAX(int(audioSeq->Size()), maxLen);
     }
+    return maxLen;
+}
+
+/* get the maximum target sentence length in a range of buffer */
+int AudioDataSetBase::MaxTgtLen(int begin, int end) {
+    CheckNTErrors((end > begin) && (begin >= 0)
+        && (end <= buf->count), "Invalid range");
+    int maxLen = 0;
+    for (int i = begin; i < end; i++) {
+        IntList* tgtSent = ((TripleSample*)buf->Get(i))->tgtSeq;
+        maxLen = MAX(int(tgtSent->Size()), maxLen);
+    }
+    return maxLen;
+}
+
+/* get the maximum source sentence length in a range of buffer */
+int AudioDataSetBase::MaxSrcLen(int begin, int end) {
+    CheckNTErrors((end > begin) && (begin >= 0)
+        && (end <= buf->count), "Invalid range");
+    int maxLen = 0;
+    for (int i = begin; i < end; i++) {
+        IntList* srcSent = ((TripleSample*)buf->Get(i))->srcSeq;
+        maxLen = MAX(int(srcSent->Size()), maxLen);
+    }
+    return maxLen;
+}
+
+/* sort the buffer by audio length (in ascending order) */
+void AudioDataSetBase::SortByAudioLengthAscending() {
+    stable_sort(buf->items, buf->items + buf->count,
+        [](void* a, void* b) {
+            return ((TripleSample*)(a))->audioSeq->Size() <
+                ((TripleSample*)(b))->audioSeq->Size();
+        });
+}
+
+/* sort the buffer by target sentence length (in ascending order) */
+void AudioDataSetBase::SortByTgtLengthAscending()
+{
+    stable_sort(buf->items, buf->items + buf->count,
+        [](void* a, void* b) {
+            return ((TripleSample*)(a))->tgtSeq->Size() <
+                ((TripleSample*)(b))->tgtSeq->Size();
+        });
+}
+
+/* sort the buffer by source sentence length (in ascending order) */
+void AudioDataSetBase::SortBySrcLengthAscending() {
+    stable_sort(buf->items, buf->items + buf->count,
+        [](void* a, void* b) {
+            return ((TripleSample*)(a))->srcSeq->Size() <
+                ((TripleSample*)(b))->srcSeq->Size();
+        });
+}
+
+/* sort the buffer by audio length (in descending order) */
+void AudioDataSetBase::SortByAudioLengthDescending() {
+    stable_sort(buf->items, buf->items + buf->count,
+        [](void* a, void* b) {
+            return ((TripleSample*)(a))->audioSeq->Size() >
+                ((TripleSample*)(b))->audioSeq->Size();
+        });
+}
+
+/* sort the buffer by target sentence length (in descending order) */
+void AudioDataSetBase::SortByTgtLengthDescending()
+{
+    stable_sort(buf->items, buf->items + buf->count,
+        [](void* a, void* b) {
+            return ((TripleSample*)(a))->tgtSeq->Size() >
+                ((TripleSample*)(b))->tgtSeq->Size();
+        });
+}
+
+/* sort the buffer by source sentence length (in descending order) */
+void AudioDataSetBase::SortBySrcLengthDescending() {
+    stable_sort(buf->items, buf->items + buf->count,
+        [](void* a, void* b) {
+            return ((TripleSample*)(a))->srcSeq->Size() >
+                ((TripleSample*)(b))->srcSeq->Size();
+        });
+}
+
+/*
+clear the buffer
+>> buf - the buffer (list) of samples
+*/
+void AudioDataSetBase::ClearBuf()
+{
+    bufIdx = 0;
+    for (int i = 0; i < buf->count; i++) {
+        TripleSample* sample = (TripleSample*)buf->Get(i);
+        delete sample;
+    }
+    buf->Clear();
+}
+
+/* constructor */
+AudioDataSetBase::AudioDataSetBase()
+{
+    fc = 0;
+    wc = 0;
+    sc = 0;
+    bufIdx = 0;
+    config = NULL;
+    buf = new XList();
+}
+
+/* de-constructor */
+AudioDataSetBase::~AudioDataSetBase()
+{
+    if (buf != NULL) {
+        ClearBuf();
+        delete buf;
+    }
+}
+
+/* constructor */
+TripleSample::TripleSample(TensorList* a, IntList* s, IntList* t, int myKey) {
+    index = -1;
+    audioSeq = a;
+    srcSeq = s;
+    tgtSeq = t;
+    bucketKey = myKey;
+}
+
+/* de-constructor */
+TripleSample::~TripleSample() {
+    if (audioSeq != NULL)
+        delete audioSeq;
+    if (srcSeq != NULL)
+        delete srcSeq;
+    if (tgtSeq != NULL)
+        delete tgtSeq;
+}
+
 }
