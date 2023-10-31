499cdc64b8fda2fb71208022a4badc78acdf9c02
Kaiqi Kou <Koukq0907@gmail.com>
2023-10-31 14:14:10 +0800

    Add S2TBeamSearch

--- source/s2t/generate/S2TSearcher.cpp
+++ source/s2t/generate/S2TSearcher.cpp
@@ -123,7 +123,7 @@ namespace s2t {
 	XTensor S2TGreedySearch::Suppress(XTensor& input)
 	{
 		XTensor modify;
-		InitTensor3D(&modify, 1, 1, 1, X_FLOAT, input.devID);
+		InitTensor3D(&modify, input.GetDim(0), 1, 1, X_FLOAT, input.devID);
 		modify = ScaleAndShift(modify, 0.0, -1e9);
 
 		if (suppressSymbolNum <= 0)
@@ -301,15 +301,6 @@ namespace s2t {
 			}
 		}
 
-		/*print output*/
-		for (int i = 0; i < batchSize; i++) {
-			cout << "batch:" << i << "output: ";
-			for (int j = 0; j < outputs[i]->count; j++) {
-				cout << outputs[i]->GetItem(j) << " ";
-			}
-			cout << endl;
-		}
-
 		cout << "--- S2TGreedySearch Search End ---" << endl;
 	}
 
@@ -325,7 +316,7 @@ namespace s2t {
 		fullHypos = NULL;
 		endSymbols = new int[32];
 		startSymbols = new int[32];
-		suppressSymbols = new int[32];
+		suppressSymbols = new int[100];
 		isEarlyStop = false;
 		needReorder = false;
 		scalarMaxLength = 0.0F;
@@ -375,6 +366,8 @@ namespace s2t {
 								47425, 49870, 50254, 50258, 50358, 50359, 50360, 50361, 50362 };
 
 		InitSuppressSymbols(config, suppressTokens, tokenNum);
+		// BeamSearch::Init((NMTConfig)(config));
+		BeamSearch::Init(endSymbols, endSymbolNum, startSymbols[0], maxLen, beamSize, batchSize, alpha, scalarMaxLength);
 	}
 
 	void S2TBeamSearch::InitStartSymbols(S2TConfig& config)
@@ -407,6 +400,135 @@ namespace s2t {
 	}
 
 	/*
+	prepare for search
+	>> batchSize - size of the batch
+	>> beamSize - size of the beam
+	*/
+	void S2TBeamSearch::Prepare(int myBatchSize, int myBeamSize)
+	{
+		batchSize = myBatchSize;
+		beamSize = myBeamSize;
+		needReorder = false;
+
+		/* prepare for the heap of hypotheses */
+		if (fullHypos != NULL)
+			delete[] fullHypos;
+
+		fullHypos = new XHeap<MIN_HEAP, float>[batchSize];
+
+		for (int i = 0; i < batchSize; i++)
+			fullHypos[i].Init(beamSize);
+
+		/* prepare for the indices of alive states */
+		aliveStatePids.Clear();
+		aliveSentList.Clear();
+		for (int i = 0; i < batchSize; i++) {
+			aliveStatePids.Add(i);
+			aliveSentList.Add(i);
+		}
+	}
+
+	/*
+	collect hypotheses with ending symbols. Given a beam of hypotheses,
+	we remove the finished hypotheses and keep them in a heap.
+	>> beam  - the beam that keeps a number of states
+	*/
+	void S2TBeamSearch::Collect(StateBundle* beam)
+	{
+		State* states = beam->states;
+
+		for (int i = 0; i < beam->stateNum; i++) {
+			State& state = states[i];
+
+			CheckNTErrors(state.pid >= 0 && state.pid < batchSize,
+				"Invalid sample id!");
+
+			/* check if this is the first end symbol. It is false
+			   if there have been end symbols in previously generated words. */
+			bool isCompleted = state.isCompleted &&
+				(state.last == NULL || !state.last->isCompleted);
+
+			/* we push the hypothesis into the heap when it is completed */
+			if ((state.isEnd || state.isCompleted)) {
+				fullHypos[state.pid].Push(HeapNode<float>(&state, state.modelScore));
+			}
+		}
+	}
+
+	/*
+	fill the hypothesis heap with incomplete hypotheses
+	>> beam  - the beam that keeps a number of states (final)
+	*/
+	void S2TBeamSearch::FillHeap(StateBundle* beam)
+	{
+		State* states = beam->states;
+
+		for (int i = 0; i < beam->stateNum / beamSize; i++) {
+			for (int j = 0; j < beamSize; j++) {
+				State& state = states[i * beamSize + j];
+
+				/* we push the incomplete hypothesis into the heap */
+				if (fullHypos[state.pid].Count() == 0) {
+					fullHypos[state.pid].Push(HeapNode<float>(&state, state.modelScore));
+				}
+				else {
+					auto node = fullHypos[state.pid].Top();
+					float score = node.value;
+					if (score < state.modelScore)
+						fullHypos[state.pid].Push(HeapNode<float>(&state, state.modelScore));
+				}
+			}
+		}
+	}
+
+	/*
+	save the output sequences in a tensor
+	>> output - output sequences (for return)
+	>> score - score of thes sequences
+	*/
+	void S2TBeamSearch::Dump(IntList** output, XTensor* score)
+	{
+		int dims[3] = { batchSize, 1 };
+
+		InitTensor(score, 2, dims, X_FLOAT);
+		score->SetZeroAll();
+
+		/* heap for an input sentence in the batch */
+		for (int h = 0; h < batchSize; h++) {
+			IntList* tgt = output[h];
+			XHeap<MIN_HEAP, float>& heap = fullHypos[h];
+			int c = heap.Count();
+
+			float bestScore = -1e9F;
+			State* state = NULL;
+			for (int i = 0; i < c; i++) {
+				auto node = heap.Pop();
+				State* s = (State*)node.index;
+				if (i == 0 || bestScore < node.value) {
+					state = s;
+					bestScore = node.value;
+				}
+			}
+
+			int count = 0;
+			bool isCompleted = true;
+
+			/* we track the state from the end to the beginning */
+			while (state != NULL) {
+				if (!state->isCompleted)
+					isCompleted = false;
+				if (!isCompleted) {
+					tgt->Add(state->prediction);
+				}
+				state = state->last;
+			}
+			tgt->Reverse();
+
+			score->Set2D(bestScore, h, 0);
+		}
+	}
+
+	/*
 	search for the most promising states
 	>> model - the transformer model
 	>> input - input of the model
@@ -414,7 +536,7 @@ namespace s2t {
 	>> outputs - outputs that represent the sequences as rows
 	>> score - score of the sequences
 	*/
-	void S2TBeamSearch::Search(S2TModel* model, XTensor& input, XTensor& padding, IntList** output, XTensor& score)
+	void S2TBeamSearch::Search(S2TModel* model, XTensor& input, XTensor& padding, IntList** outputs, XTensor& score)
 	{
 		cout << "----- S2TBeamSearch Search -----" << endl;
 		
@@ -432,6 +554,280 @@ namespace s2t {
 
 		/* encoder mask */
 		model->MakeS2TMaskEnc(padding, maskEnc);
+
+		/* make the encoding network */
+		cout << "----- Encoding -----" << endl;
+		if (model->config->model.encPreLN)
+			encoding = model->encoder->RunFastPreNorm(input, &maskEnc);
+		// else
+			/*TODO*/
+			// encoding = model->encoder->RunFastPostNorm(input, &maskEnc);
+		cout << "--- Encoding End ---" << endl;
+
+		encodingBeam = Unsqueeze(encoding, encoding.order - 2, beamSize);
+		inputBeam = Unsqueeze(input, input.order - 2, beamSize);
+		paddingBeam = Unsqueeze(padding, padding.order - 1, beamSize);
+
+		encodingBeam.ReshapeMerged(encodingBeam.order - 4);
+		inputBeam.ReshapeMerged(inputBeam.order - 4);
+		paddingBeam.ReshapeMerged(paddingBeam.order - 3);
+
+		/* max output-length = scalar * source-length */
+		int lengthLimit = MIN(int(float(input.GetDim(-2)) * scalarMaxLength), maxLen);
+		CheckNTErrors(lengthLimit > 0, "Invalid maximum output length");
+		cout << "lengthLimit: " << lengthLimit << endl;
+
+		StateBundle* states = new StateBundle[lengthLimit + 1];
+		StateBundle* first = states;
+		StateBundle* cur = NULL;
+		StateBundle* next = NULL;
+
+		/* create the first state */
+		predictor.Init(endSymbols, endSymbolNum, startSymbols, startSymbolNum, suppressSymbols, suppressSymbolNum);
+		predictor.Create(&input, beamSize, first);
+		
+		first->isStart = true;
+
+		XTensor aliveState;
+		InitTensor1D(&aliveState, batchSize * beamSize, X_INT, input.devID);
+		SetAscendingOrder(aliveState, 0);
+
+		XTensor reorderState;
+		InitTensor1D(&reorderState, batchSize * beamSize, X_INT, input.devID);
+		SetAscendingOrder(reorderState, 0);
+
+		model->decoder->embedder->scale = FALSE;
+
+		/* generate the sequence from left to right */
+		// lengthLimit = 1;
+		for (int l = 0; l < lengthLimit; l++) {
+
+			int nstep = l;
+			if (l > 0)
+				nstep += (startSymbolNum - 1);
+			
+			if (beamSize > 1) {
+				inputBeam = AutoGather(inputBeam, reorderState);
+				paddingBeam = AutoGather(paddingBeam, reorderState);
+				encodingBeam = AutoGather(encodingBeam, reorderState);
+			}
+
+			cur = states + l;
+			next = states + l + 1;
+
+			/* read the current state */
+			predictor.Read(model, cur);
+
+			/* predict the next state */
+			predictor.Predict(next, aliveState, encodingBeam, inputBeam,
+				paddingBeam, batchSize * beamSize, l == 0, reorderState, needReorder, nstep);
+
+			/* compute the model score (given the prediction probability) */
+			Score(cur, next);
+
+			next->prob.enableGrad = false;
+			next->probPath.enableGrad = false;
+			next->modelScore.enableGrad = false;
+
+			/* beam pruning */
+			Generate(cur, next);
+
+			/* expand the search graph */
+			Expand(cur, next, reorderState);
+
+			/* push complete hypotheses into the heap */
+			Collect(next);
+
+			next->prediction.Dump(stderr, "\prediction: ", -1);
+			next->probPath.Dump(stderr, "probPath: ", -1);
+
+			/* stop searching when all hypotheses are completed */
+			if (IsAllCompleted(next)) {
+				break;
+			}
+
+			/* remove finished sentences */
+			//RemoveFinishedStates(next, encodingBeam, inputBeam, paddingBeam, aliveState);
+		}
+
+		/* fill the heap with incomplete hypotheses if necessary */
+		FillHeap(next);
+
+		Dump(outputs, &score);
+
+		delete[] states;
+
+		cout << "--- S2TBeamSearch Search End ---" << endl;
+	}
+
+	S2TPredictor::S2TPredictor()
+	{
+		m = NULL;
+		s = NULL;
+		endSymbols = NULL;
+		endSymbolNum = 0;
+		startSymbols = NULL;
+		startSymbolNum = 0;
+		suppressSymbols = NULL;
+		suppressSymbolNum = 0;
+	}
+
+	S2TPredictor::~S2TPredictor()
+	{
+		m = NULL;
+		s = NULL;
+		endSymbols = NULL;
+		startSymbols = NULL;
+		suppressSymbols = NULL;
+	}
+
+	void S2TPredictor::Init(int* endS, int endN, int* startS, int startN, int* suppS, int suppN)
+	{
+		endSymbols = endS;
+		endSymbolNum = endN;
+		startSymbols = startS;
+		startSymbolNum = startN;
+		suppressSymbols = suppS;
+		suppressSymbolNum = suppN;
+	}
+
+	/*
+	create an initial state
+	>> model - the  model
+	>> top - the top-most layer of the network
+	>> input - input of the network
+	>> beamSize - beam size
+	>> state - the state to be initialized
+	*/
+	void S2TPredictor::Create(const XTensor* input, int beamSize, StateBundle* state)
+	{
+		/*TODO*/
+		int dims[MAX_TENSOR_DIM_NUM];
+		dims[0] = input->dimSize[0];
+		dims[1] = beamSize;
+
+		InitTensor(&state->probPath, input->order-1, dims, X_FLOAT, input->devID);
+		InitTensor(&state->endMark, input->order-1, dims, X_INT, input->devID);
+
+		state->probPath.SetZeroAll();
+		state->nstep = 0.0F;
+		state->endMark.SetZeroAll();
+
+		state->stateNum = 0;
+	}
+
+	/*
+	read a state
+	>> model - the  model that keeps the network created so far
+	>> state - a set of states. It keeps
+	1) hypotheses (states)
+	2) probabilities of hypotheses
+	3) parts of the network for expanding toward the next state
+	*/
+	void S2TPredictor::Read(S2TModel* model, StateBundle* state)
+	{
+		m = model;
+		s = state;
+	}
+
+	XTensor S2TPredictor::Suppress(XTensor& input)
+	{
+		XTensor modify;
+		InitTensor3D(&modify, input.GetDim(0), 1, 1, X_FLOAT, input.devID);
+		modify = ScaleAndShift(modify, 0.0, -1e9);
+
+		if (suppressSymbolNum <= 0)
+			return input;
+
+		for (int i = 0; i < suppressSymbolNum; i++) {
+			_SetDataIndexed(&input, &modify, input.order - 1, suppressSymbols[i]);
+		}
+
+		return input;
+	}
+
+	void S2TPredictor::Predict(StateBundle* next, XTensor& aliveState, XTensor& encoding,
+		XTensor& inputEnc, XTensor& paddingEnc, int batchSize, bool isStart,
+		XTensor& reorderState, bool needReorder, int nstep)
+	{
+		int dims[MAX_TENSOR_DIM_NUM];
+
+		/* word indices of positions up to next state */
+		XTensor inputDec;
+
+		/* the first token */
+		XTensor first;
+		InitTensor1D(&first, startSymbolNum, X_INT, inputEnc.devID);
+		first.SetData(startSymbols, startSymbolNum);
+		first = Unsqueeze(first, 0, batchSize);
+
+		/* add a new word into the input sequence of the decoder side */
+		if (isStart) {
+			inputDec = Identity(first);
+		}
+		else {
+			/* only pass one step to the decoder */
+			inputDec = GetLastPrediction(s, inputEnc.devID);
+		}
+
+		/* keep alive states for the decoder */
+		if (aliveState.dimSize[0] < batchSize) {
+			/* alive inputs */
+			inputDec = AutoGather(inputDec, aliveState);
+
+			/* alive cache */
+			for (int i = 0; i < m->decoder->nlayer; i++) {
+				m->decoder->selfAttCache[i].KeepAlive(aliveState);
+				m->decoder->enDeAttCache[i].KeepAlive(aliveState);
+			}
+		}
+
+		if (needReorder) {
+			for (int i = 0; i < m->decoder->nlayer; i++) {
+				m->decoder->selfAttCache[i].Reorder(reorderState);
+				m->decoder->enDeAttCache[i].Reorder(reorderState);
+			}
+		}
+
+		/* prediction probabilities */
+		XTensor& output = next->prob;
+		XTensor decoding;
+
+		for (int i = 0; i < inputDec.order - 1; i++)
+			dims[i] = inputDec.dimSize[i];
+		dims[inputDec.order - 1] = inputDec.dimSize[inputDec.order - 1];
+
+		XTensor maskDec;
+
+		/* decoder mask */
+		maskDec = m->MakeS2TTriMaskDecInference(batchSize, inputDec.GetDim(-1));
+
+		/* make the decoding network */
+		if (m->config->model.decPreLN)
+			if (isStart)
+				decoding = m->decoder->RunFastPreNorm(inputDec, encoding, &maskDec, NULL, nstep);
+			else
+				decoding = m->decoder->RunFastPreNorm(inputDec, encoding, NULL, NULL, nstep);
+		
+		/* TODO
+		else
+			decoding = m->decoder->RunFastPostNorm(inputDec, encoding, &maskEncDec, nstep);*/
+
+		CheckNTErrors(decoding.order >= 2, "The tensor must be of order 2 or larger!");
+
+		/* generate the output probabilities */
+		bool outputSoftmax = FALSE;
+		if (outputSoftmax)
+			output = m->outputLayer->Make(decoding, false);
+		else
+			output = MMul(decoding, X_NOTRANS, *m->outputLayer->w, X_TRANS);
+
+		/*only consider the last token*/
+		output = SelectRange(output, 1, output.GetDim(1) - 1, output.GetDim(1));
+
+		output = Suppress(output);
+		
+		output = LogSoftmax(output, -1);
 	}
 
 }
